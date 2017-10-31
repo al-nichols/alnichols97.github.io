f6914cc9c225c196c88cc8350aa3edde3f375dbc
Alexander Nichols <alexandernichols1025@gmail.com>
2017-10-31 16:04:55 -0400

    Comment functionality working
    Woo!

--- app.js
+++ app.js
@@ -9,13 +9,20 @@ function($stateProvider, $urlRouterProvider) {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
+    })
+
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
     });
+
     $urlRouterProvider.otherwise('home');
 
 }])
 .factory('posts', [function(){
   var o = {
-    posts: [{title: 'hello', link:'', upvotes:0}]
+    posts: []
   };
   return o;
 }])
@@ -31,7 +38,11 @@ function($scope, posts){
     $scope.posts.push({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0
+      upvotes: 0,
+      comments: [
+        {'author': 'Joe (Not a robot...)', body: 'BLEEP BLOOP GOOD POST!', upvotes: 0},
+        {'author': 'Bob (Definitely Not a robot...)', body: 'BLOOP BLOP DESTROY!!!', upvotes: 0},
+      ]
     });
     $scope.title = '';
     $scope.link = '';
@@ -40,4 +51,21 @@ function($scope, posts){
   $scope.incrementUpvotes = function(post){
     post.upvotes += 1;
   }
+}])
+.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+function($scope, $stateParams, posts) {
+  $scope.post = posts.posts[$stateParams.id];
+
+  $scope.addComment = function(){
+    if ($scope.body === '') { return; }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0,
+    });
+    $scope.body = '';
+}
 }]);
